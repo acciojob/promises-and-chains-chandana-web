893371c31609f4eb52a3b3f6a4bc8eb36759d806
chandana-web <143087553+chandana-web@users.noreply.github.com>
2025-01-17 18:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const vName=document.getElementById('name');
+const vAge=document.getElementById('age');
+const buttonId= document.getElementById('btn');
+
+buttonId.addEventListener('click', async()=>{
+
+const tAge= parseInt(vAge.value);
+const tName= vName.value.trim();
+
+const promise1= new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		if(tAge>= 18) resolve(alert(`Welcome,  ${tName}. You can vote.`));
+    else reject(alert(`Oh sorry ${tName}. You aren't old enough.`));
+	}, 4000);
+}
+);
+	try {
+		const data= await promise1;
+		alert(data)
+	} catch (error) {
+		alert(error);
+	}
+});
